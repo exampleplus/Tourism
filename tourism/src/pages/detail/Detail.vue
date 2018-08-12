17aa9f60f1b582206cde59a39cdb7967d8638adb
AFODC-708191223\Administrator <384868059@qq.com>
2018-08-12 22:21:44 +0800

    头部渐隐渐现，滚动事件解绑

--- tourism/src/pages/detail/Detail.vue
+++ tourism/src/pages/detail/Detail.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+	<detail-header></detail-header>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 import axios from 'axios'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+	DetailHeader
   },
   data () {
     return {
